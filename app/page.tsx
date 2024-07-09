d69e2492d61b10a392f590097fe271320d4a1c51
sipamungkas <sipamungkas.dev@gmail.com>
2024-07-09 22:34:48 +0700

    feat: add hero section, and experience section

--- app/page.tsx
+++ app/page.tsx
@@ -1,55 +1,135 @@
 import { Link } from "@nextui-org/link";
 import { Snippet } from "@nextui-org/snippet";
 import { Code } from "@nextui-org/code";
+import { Image } from "@nextui-org/image";
+import { Divider } from "@nextui-org/divider";
 import { button as buttonStyles } from "@nextui-org/theme";
 
-import { siteConfig } from "@/config/site";
 import { title, subtitle } from "@/components/primitives";
-import { GithubIcon } from "@/components/icons";
+import NextImage from "next/image";
 
 export default function Home() {
   return (
-    <section className="flex flex-col items-center justify-center gap-4 py-8 md:py-10">
-      <div className="inline-block max-w-lg text-center justify-center">
-        <h1 className={title()}>Make&nbsp;</h1>
-        <h1 className={title({ color: "violet" })}>beautiful&nbsp;</h1>
-        <br />
-        <h1 className={title()}>
-          websites regardless of your design experience.
-        </h1>
-        <h2 className={subtitle({ class: "mt-4" })}>
-          Beautiful, fast and modern React UI library.
-        </h2>
+    <section className="flex flex-col items-center justify-center gap-4 py-8 md:py-10 md:px-12">
+      <div className="flex justify-between">
+        <div className="w-[60%]">
+          <div className="inline-block">
+            <h1 className={title()}>Hi, I am&nbsp;</h1>
+            <h1 className={title({ color: "violet" })}>Ragil&nbsp;</h1>
+
+            <h4 className="mt-3 text-slate-500 dark:text-slate-400 text-md">
+              Welcome to my space!
+            </h4>
+          </div>
+          <p className="mt-3">
+            I am an experienced{" "}
+            <span className="tracking-tight inline font-semibold from-[#FF1CF7] to-[#b249f8] bg-clip-text text-transparent bg-gradient-to-b">
+              React Native
+            </span>{" "}
+            and{" "}
+            <span className="tracking-tight inline font-semibold from-[#FF1CF7] to-[#b249f8] bg-clip-text text-transparent bg-gradient-to-b">
+              web developer{" "}
+            </span>{" "}
+            with over 3 years of hands-on experience in building custom mobile
+            apps and custom website. My expertise lies in creating high-quality,
+            scalable solutions that meet the unique needs of each project.
+          </p>
+        </div>
+        <Image
+          as={NextImage}
+          isBlurred
+          width={150}
+          height={150}
+          src="/images/me.jpg"
+          alt="Ragil's Photo"
+          className="m-5"
+          radius="full"
+        />
       </div>
 
-      <div className="flex gap-3">
-        <Link
-          isExternal
-          className={buttonStyles({
-            color: "primary",
-            radius: "full",
-            variant: "shadow",
-          })}
-          href={siteConfig.links.docs}
-        >
-          Documentation
-        </Link>
-        <Link
-          isExternal
-          className={buttonStyles({ variant: "bordered", radius: "full" })}
-          href={siteConfig.links.github}
-        >
-          <GithubIcon size={20} />
-          GitHub
-        </Link>
-      </div>
+      <div className="mt-8 lg:mt-20 w-full">
+        <div className="flex justify-between items-center">
+          <h2 className={title({ size: "sm" })}>Experience</h2>
+          <Link
+            isExternal
+            size="lg"
+            className={buttonStyles({
+              color: undefined,
+              radius: "full",
+              variant: "ghost",
+            })}
+            href={"/resume.pdf"}
+          >
+            Resume
+          </Link>
+        </div>
+        <div>
+          <h3 className="mt-8 text-lg md:text-xl font-semibold mb-4">
+            React Native Developer @Telkom Indonesia
+          </h3>
+          <p className="text-sm md:text-base text-slate-500 dark:text-slate-400 mb-3 italic">
+            August 2021 - Present
+          </p>
+          <ul className=" marker:text-[#b249f8] list-outside list-disc ml-6 gap-1 flex flex-col text-lg font-normal">
+            <li>
+              Developed new functionality to support existing mobile
+              application.
+            </li>
+            <li>
+              Developed and maintained highly scalable android and ios apps
+              using React Native, Typescript, Jest, firebase and Redux.
+            </li>
+            <li>
+              Actively participated in daily standups, project breakdown,
+              estimation, and retrospective meetings.
+            </li>
+            <li>Enhancing and Optimizing the Payment and Billing Feature.</li>
+          </ul>
+          <Divider className="mt-8 md:mt-12 h-0.5" />
+        </div>
 
-      <div className="mt-8">
-        <Snippet hideCopyButton hideSymbol variant="bordered">
-          <span>
-            Get started by editing <Code color="primary">app/page.tsx</Code>
-          </span>
-        </Snippet>
+        <div>
+          <h3 className="mt-8 text-lg md:text-xl font-semibold mb-4">
+            Fullstack Laravel Developer @PT Pan Brothers Tbk
+          </h3>
+          <p className="text-sm md:text-base text-slate-500 dark:text-slate-400 mb-3 italic">
+            January 2020 - January 2021
+          </p>
+          <ul className=" marker:text-[#b249f8] list-outside list-disc ml-6 gap-1 flex flex-col text-lg font-normal">
+            <li>
+              Developed Human Resource information and personalia system and
+              many other internal system
+            </li>
+            <li>
+              Developed an app to track employee attendance, calculate working
+              hours and overtime, and manage payroll efficiently.
+            </li>
+            <li>Actively participated in code improvement and new feature.</li>
+          </ul>
+          <Divider className="mt-8 md:mt-12 h-0.5" />
+        </div>
+
+        <div>
+          <h3 className="mt-8 text-lg md:text-xl font-semibold mb-4">
+            React Native Mobile Developer @PT Kreasi Kode Indonesia
+          </h3>
+          <p className="text-sm md:text-base text-slate-500 dark:text-slate-400 mb-3 italic">
+            2019
+          </p>
+          <ul className=" marker:text-[#b249f8] list-outside list-disc ml-6 gap-1 flex flex-col text-lg font-normal">
+            <li>
+              Developed custom mobile applications using the React Native
+              framework, seamlessly integrating business logic and UI/UX designs
+              provided by the UI/UX Division.
+            </li>
+            <li>
+              Successfully delivered the 'Master Event' app for The ICIFPRH
+              2019, utilizing React Native to meet all functional and aesthetic
+              requirements.
+            </li>
+          </ul>
+          <Divider className="mt-8 md:mt-12 h-0.5" />
+        </div>
       </div>
     </section>
   );
--- components/navbar.tsx
+++ components/navbar.tsx
@@ -50,11 +50,11 @@ export const Navbar = () => {
 
   return (
     <NextUINavbar maxWidth="xl" position="sticky">
-      <NavbarContent className="basis-1/5 sm:basis-full" justify="start">
+      <NavbarContent className="basis-1/5 sm:basis-full md:px-12" justify="start">
         <NavbarBrand as="li" className="gap-3 max-w-fit">
           <NextLink className="flex justify-start items-center gap-1" href="/">
             <Logo />
-            <p className="font-bold text-inherit">ACME</p>
+            <p className="font-bold text-inherit">Sipamungkas</p>
           </NextLink>
         </NavbarBrand>
         <ul className="hidden lg:flex gap-4 justify-start ml-2">
@@ -63,7 +63,7 @@ export const Navbar = () => {
               <NextLink
                 className={clsx(
                   linkStyles({ color: "foreground" }),
-                  "data-[active=true]:text-primary data-[active=true]:font-medium",
+                  "data-[active=true]:text-primary data-[active=true]:font-medium"
                 )}
                 color="foreground"
                 href={item.href}
@@ -76,37 +76,18 @@ export const Navbar = () => {
       </NavbarContent>
 
       <NavbarContent
-        className="hidden sm:flex basis-1/5 sm:basis-full"
+        className="hidden sm:flex basis-1/5 sm:basis-full md:px-12"
         justify="end"
       >
         <NavbarItem className="hidden sm:flex gap-2">
-          <Link isExternal aria-label="Twitter" href={siteConfig.links.twitter}>
-            <TwitterIcon className="text-default-500" />
-          </Link>
-          <Link isExternal aria-label="Discord" href={siteConfig.links.discord}>
-            <DiscordIcon className="text-default-500" />
-          </Link>
           <Link isExternal aria-label="Github" href={siteConfig.links.github}>
             <GithubIcon className="text-default-500" />
           </Link>
           <ThemeSwitch />
         </NavbarItem>
-        <NavbarItem className="hidden lg:flex">{searchInput}</NavbarItem>
-        <NavbarItem className="hidden md:flex">
-          <Button
-            isExternal
-            as={Link}
-            className="text-sm font-normal text-default-600 bg-default-100"
-            href={siteConfig.links.sponsor}
-            startContent={<HeartFilledIcon className="text-danger" />}
-            variant="flat"
-          >
-            Sponsor
-          </Button>
-        </NavbarItem>
       </NavbarContent>
 
-      <NavbarContent className="sm:hidden basis-1 pl-4" justify="end">
+      <NavbarContent className="sm:hidden basis-1 pl-4 md:px-12" justify="end">
         <Link isExternal aria-label="Github" href={siteConfig.links.github}>
           <GithubIcon className="text-default-500" />
         </Link>
@@ -124,8 +105,8 @@ export const Navbar = () => {
                   index === 2
                     ? "primary"
                     : index === siteConfig.navMenuItems.length - 1
-                      ? "danger"
-                      : "foreground"
+                    ? "danger"
+                    : "foreground"
                 }
                 href="#"
                 size="lg"
